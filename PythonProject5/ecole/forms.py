21c0cbb755a7a53fd9f5b8b47dd55a9da507f544
Lucas-hrl <lucas.herchuel@uha.fr>
2025-06-12 16:55:23 +0200

    v3

--- PythonProject5/ecole/forms.py
+++ PythonProject5/ecole/forms.py
@@ -7,6 +7,10 @@ class GroupeForm(forms.ModelForm):
         fields = ['nom_gr']
         labels = {'nom_gr': 'Nom du groupe'}
 
+        widgets = {
+            'nom_gr': forms.TextInput(attrs={'class': 'class_css_input', 'placeholder': 'Nom du groupe…'}),
+        }
+
 class EtudiantForm(forms.ModelForm):
     class Meta:
         model = Etudiants
@@ -19,6 +23,14 @@ class EtudiantForm(forms.ModelForm):
             'photo': 'Photo'
         }
 
+        widgets = {
+            'nom_etu': forms.TextInput(attrs={'class': 'class_css_input', "placeholder': 'Nom de l'étudiant…"}),
+            'prenom_etu': forms.TextInput(attrs={'class': 'class_css_input', "placeholder': 'Prénom de l'étudiant…"}),
+            'mail_etu': forms.EmailInput(),
+            'groupe': forms.Select(attrs={'class': 'class_css_input'}),
+            'photo': forms.FileInput(attrs={'class': 'class_css_input'}),
+        }
+
 class EnseignantForm(forms.ModelForm):
     class Meta:
         model = Enseignants
@@ -27,6 +39,11 @@ class EnseignantForm(forms.ModelForm):
             'nom_ens': 'Nom',
             'prenom_ens': 'Prénom',
             'mail_ens': 'E-mail'
+        }
+        widgets = {
+            'nom_ens': forms.TextInput(attrs={'class': 'class_css_input', "placeholder': 'Nom de l'enseignant…"}),
+            'prenom_ens': forms.TextInput(attrs={'class': 'class_css_input', "placeholder': 'Prénom de l'enseignant…"}),
+            'mail_ens': forms.EmailInput(),
         }
 
 class CoursForm(forms.ModelForm):
@@ -40,6 +57,13 @@ class CoursForm(forms.ModelForm):
             'groupe': 'Groupe',
             'duree_cours': 'Durée'
         }
+        widgets = {
+            'titre_cours': forms.TextInput(attrs={'class': 'class_css_input', "placeholder': 'Titre du cours…"}),
+            'date_cours': forms.DateInput(),
+            'enseignants': forms.Select(attrs={'class': 'class_css_input'}),
+            'groupe': forms.Select(attrs={'class': 'class_css_input'}),
+            'duree_cours': forms.TimeInput(),
+        }
 
 class AbsenceForm(forms.ModelForm):
     class Meta:
@@ -50,4 +74,10 @@ class AbsenceForm(forms.ModelForm):
             'cours': 'Cours',
             'justifie': 'Justifié',
             'justification': 'Justification'
+        }
+        widgets = {
+            'etudiants':forms.Select(attrs={'class': 'class_css_input'}),
+            'cours':forms.Select(attrs={'class': 'class_css_input'}),
+            'justifie':forms.CheckboxInput(),
+            'justification':forms.FileInput(attrs={'class': 'class_css_input'}),
         }
