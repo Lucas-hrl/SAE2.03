8cfa4bf550ce9d9f140adbff500ed623ee64edb7
Lucas-hrl <lucas.herchuel@uha.fr>
2025-06-13 09:39:14 +0200

    v3

--- PythonProject5/ecole/urls.py
+++ PythonProject5/ecole/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     # Index/page d'accueil
-    path("", views.index),
+    path("", views.index, name="index"),
+    path("ajouter_groupe/", views.ajouter_groupe, name="ajouter_groupe"),
+    path("traitement_groupe/",views.traitement_groupe,name="traitement_groupe"),
+    path("liste_groupes/",views.liste_groupes,name="liste_groupes"),
+    path("afficher_groupe/<int:id>/", views.afficher_groupe),
+    path("update_groupe/<int:id>/",views.update_groupe),
+    path("traitement_update_groupe/<int:id>/",views.traitement_update_groupe),
+    path("supprimer_groupe/<int:id>/",views.supprimer_groupe),
 ]
--- PythonProject5/ecole/views.py
+++ PythonProject5/ecole/views.py
@@ -4,7 +4,50 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from . import models
 
-# Groupes
+# Page d'accueil
 def index (request):
     return render(request,"ecole/index.html")
+
+#Groupes
+def ajouter_groupe(request):
+    form = GroupeForm()
+    return render(request, "ecole/ajouter_groupe.html", {"form": form})
+
+def traitement_groupe(request):
+    form = GroupeForm(request.POST)
+    if form.is_valid():
+        Groupe = form.save()
+        return HttpResponseRedirect("/ecole/liste_groupes")
+    else:
+        return render(request,"ecole/ajouter_groupe.html/", {"form":form})
+
+def liste_groupes(request):
+    liste_groupe = list(models.Groupe.objects.all())
+    return render(request, "ecole/liste_groupes.html", {"liste_g":liste_groupe})
+
+def afficher_groupe(request,id):
+    groupe= models.Groupe.objects.get(pk=id)
+    return render (request, "ecole/afficher_groupe.html", {"groupe": groupe})
+
+def update_groupe(request,id):
+    groupe = models.Groupe.objects.get(pk=id)
+    dico= groupe.dico()
+    form = GroupeForm(dico)
+    return render(request,"ecole/ajouter_groupe.html", {"form":form, "id":id})
+
+def traitement_update_groupe (request,id):
+    gform = GroupeForm(request.POST)
+    if gform.is_valid():
+        Groupe = gform.save(commit = False)
+        Groupe.id = id
+        Groupe.save()
+        return HttpResponseRedirect("/ecole/liste_groupes")
+    else:
+        return render(request, "ecole/ajouter_groupe.html", {"gform": gform , "id":id})
+
+def supprimer_groupe(request,id):
+    groupe = models.Groupe.objects.get(pk=id)
+    groupe.delete()
+    return HttpResponseRedirect("/ecole/liste_groupes")
+
 
