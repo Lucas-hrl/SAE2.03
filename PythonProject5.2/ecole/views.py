13158d1cfecc6224130cd0746bf9df904f97677d
drknath <friedrich.nathanael@gmail.com>
2025-06-18 11:50:43 +0200

    cccc

--- PythonProject5.2/ecole/views.py
+++ PythonProject5.2/ecole/views.py
@@ -1,6 +1,6 @@
 from .models import Groupe, Etudiants, Enseignants, Cours, Absences
 from .forms import GroupeForm, EtudiantForm, EnseignantForm, CoursForm, AbsenceForm
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from . import models
 
