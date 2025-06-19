df75925c136d2d8faa011bf6eaf710d49a8b15f3
Lucas-hrl <lucas.herchuel@uha.fr>
2025-06-19 17:59:34 +0200

    Add files via upload

---
+++ PythonProjectFinal/ecole/models.py
@@ -0,0 +1,154 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+from django.core.validators import EmailValidator
+
+class Groupe(models.Model):
+    id_gr = models.AutoField(primary_key=True)
+    nom_gr = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.id_gr:
+            max_id = Groupe.objects.all().aggregate(models.Max('id_gr'))['id_gr__max']
+            self.id_gr = 1 if max_id is None else max_id + 1
+        super(Groupe, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nom_gr
+
+    class Meta:
+        managed = True
+        db_table = 'groupe'
+
+    def dico(self):
+        return {
+            'id_gr': self.id_gr,
+            'nom_gr': self.nom_gr
+        }
+
+
+class Etudiants(models.Model):
+    id_etu = models.AutoField(primary_key=True)
+    nom_etu = models.CharField(max_length=100)
+    prenom_etu = models.CharField(max_length=100)
+    mail_etu = models.EmailField(unique=True, max_length=150)
+    groupe = models.ForeignKey(Groupe, models.DO_NOTHING, db_column='groupe', blank=True, null=True)
+    photo = models.ImageField(upload_to="etudiants/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.prenom_etu} {self.nom_etu}"
+
+    def save(self, *args, **kwargs):
+        if not self.id_etu:
+            max_id = Etudiants.objects.all().aggregate(models.Max('id_etu'))['id_etu__max']
+            self.id_etu = 1 if max_id is None else max_id + 1
+        super(Etudiants, self).save(*args, **kwargs)
+
+    def dico(self):
+        return {
+            'id_etu': self.id_etu,
+            'nom_etu': self.nom_etu,
+            'prenom_etu': self.prenom_etu,
+            'mail_etu': self.mail_etu,
+            'groupe': self.groupe.dico() if self.groupe else None,
+            'photo': self.photo
+        }
+
+    class Meta:
+        managed = True
+        db_table = 'etudiants'
+
+
+class Enseignants(models.Model):
+    id_ens = models.AutoField(primary_key=True)
+    nom_ens = models.CharField(max_length=100)
+    prenom_ens = models.CharField(max_length=100)
+    mail_ens = models.EmailField(unique=True, max_length=150)
+
+    def __str__(self):
+        return f"{self.prenom_ens} {self.nom_ens}"
+
+    def save(self, *args, **kwargs):
+        if not self.id_ens:
+            max_id = Enseignants.objects.all().aggregate(models.Max('id_ens'))['id_ens__max']
+            self.id_ens = 1 if max_id is None else max_id + 1
+        super(Enseignants, self).save(*args, **kwargs)
+
+    def dico(self):
+        return {
+            'id_ens': self.id_ens,
+            'nom_ens': self.nom_ens,
+            'prenom_ens': self.prenom_ens,
+            'mail_ens': self.mail_ens
+        }
+
+    class Meta:
+        managed = True
+        db_table = 'enseignants'
+
+
+class Cours(models.Model):
+    id_cours = models.AutoField(primary_key=True)
+    titre_cours = models.CharField(max_length=150)
+    date_cours = models.DateField()
+    enseignants = models.ForeignKey(Enseignants, models.DO_NOTHING, db_column='enseignants', blank=True, null=True)
+    groupe = models.ForeignKey(Groupe, models.DO_NOTHING, db_column='groupe', blank=True, null=True)
+    duree_cours = models.TimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id_cours:
+            max_id = Cours.objects.all().aggregate(models.Max('id_cours'))['id_cours__max']
+            self.id_cours = 1 if max_id is None else max_id + 1
+        super(Cours, self).save(*args, **kwargs)
+
+    def dico(self):
+        return {
+            'id_cours': self.id_cours,
+            'titre_cours': self.titre_cours,
+            'date_cours': self.date_cours,
+            'enseignants': self.enseignants.dico() if self.enseignants else None,
+            'groupe': self.groupe.dico() if self.groupe else None,
+            'duree_cours': self.duree_cours
+        }
+
+    def __str__(self):
+        return f"{self.titre_cours} - {self.date_cours}"
+
+    class Meta:
+        managed = True
+        db_table = 'cours'
+
+
+class Absences(models.Model):
+    id_absence = models.AutoField(primary_key=True)
+    etudiants = models.ForeignKey(Etudiants, models.DO_NOTHING, db_column='etudiants', blank=True, null=True)
+    cours = models.ForeignKey(Cours, models.DO_NOTHING, db_column='cours', blank=True, null=True)
+    justifie = models.BooleanField(blank=True, null=True)
+    justification = models.FileField(upload_to='justification/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Absence de {self.etudiants} au cours {self.cours}"
+
+    def save(self, *args, **kwargs):
+        if not self.id_absence:
+            max_id = Absences.objects.all().aggregate(models.Max('id_absence'))['id_absence__max']
+            self.id_absence = 1 if max_id is None else max_id + 1
+        super(Absences, self).save(*args, **kwargs)
+
+    def dico(self):
+        return {
+            'id_absence': self.id_absence,
+            'etudiants': self.etudiants.dico() if self.etudiants else None,
+            'cours': self.cours.dico() if self.cours else None,
+            'justification': self.justification,
+            'justifie': self.justifie,
+        }
+
+    class Meta:
+        managed = True
+        db_table = 'absences'
---
+++ PythonProjectFinal/ecole/views.py
@@ -0,0 +1,237 @@
+from .models import Groupe, Etudiants, Enseignants, Cours, Absences
+from .forms import GroupeForm, EtudiantForm, EnseignantForm, CoursForm, AbsenceForm
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from . import models
+
+# Page d'accueil
+def index (request):
+    return render(request,"ecole/index.html")
+
+
+#Groupes
+def ajouter_groupe(request):
+    form = GroupeForm()
+    return render(request, "ecole/ajouter_groupe.html", {"form": form})
+
+def traitement_groupe(request):
+    form = GroupeForm(request.POST)
+    if form.is_valid():
+        Groupe = form.save()
+        return HttpResponseRedirect("/ecole/liste_groupes")
+    else:
+        return render(request,"ecole/ajouter_groupe.html/", {"form":form})
+
+def liste_groupes(request):
+    liste_groupe = list(models.Groupe.objects.all())
+    return render(request, "ecole/liste_groupes.html", {"liste_g":liste_groupe})
+
+def afficher_groupe(request,id):
+    groupe= models.Groupe.objects.get(id_gr=id)
+    return render (request, "ecole/afficher_groupe.html", {"groupe": groupe})
+
+def update_groupe(request,id):
+    groupe = models.Groupe.objects.get(id_gr=id)
+    form = GroupeForm(instance = groupe)
+    return render(request,"ecole/ajouter_groupe.html", {"form":form, "id":id})
+
+def traitement_update_groupe(request, id):
+    groupe_existant = models.Groupe.objects.get(id_gr=id)
+    gform = GroupeForm(request.POST, instance=groupe_existant)  # Utilisation de instance
+    if gform.is_valid():
+        groupe = gform.save()
+        return HttpResponseRedirect("/ecole/liste_groupes")
+    else:
+        return render(request, "ecole/ajouter_groupe.html", {"gform": gform, "id": id})
+
+def supprimer_groupe(request,id):
+    groupe = models.Groupe.objects.get(id_gr=id)
+    groupe.delete()
+    return HttpResponseRedirect("/ecole/liste_groupes")
+
+
+#Etudiants
+def ajouter_etudiant(request):
+    form = EtudiantForm()
+    return render(request, "ecole/ajouter_etudiant.html", {"form": form})
+
+def traitement_etudiant(request):
+    form = EtudiantForm(request.POST, request.FILES)
+    if form.is_valid():
+        Etudiants = form.save()
+        return HttpResponseRedirect("/ecole/liste_etudiants")
+    else:
+        return render(request,"ecole/ajouter_etudiant.html/", {"form":form})
+
+def liste_etudiants(request):
+    liste_etudiants = list(models.Etudiants.objects.all())
+    return render(request, "ecole/liste_etudiants.html", {"liste_e":liste_etudiants})
+
+def afficher_etudiant(request,id):
+    etudiant= models.Etudiants.objects.get(id_etu=id)
+    return render (request, "ecole/afficher_etudiant.html", {"etudiant": etudiant})
+
+def update_etudiant(request,id):
+    etudiant = models.Etudiants.objects.get(id_etu=id)
+    dico= etudiant.dico()
+    form = EtudiantForm(dico)
+    return render(request,"ecole/ajouter_etudiant.html", {"form":form, "id":id})
+
+def traitement_update_etudiant(request, id):
+    etudiant_existant = models.Etudiants.objects.get(id_etu=id)
+    eform = EtudiantForm(request.POST,request.FILES, instance=etudiant_existant)  # Utilisation de instance
+    if eform.is_valid():
+        etudiant = eform.save()
+        return HttpResponseRedirect("/ecole/liste_etudiants")
+    else:
+        return render(request, "ecole/ajouter_etudiant.html", {"eform": eform, "id": id})
+
+def supprimer_etudiant(request,id):
+    etudiant = models.Etudiants.objects.get(id_etu=id)
+    etudiant.delete()
+    return HttpResponseRedirect("/ecole/liste_etudiants")
+
+
+#Enseignant
+def ajouter_enseignant(request):
+    form = EnseignantForm()
+    return render(request, "ecole/ajouter_enseignant.html", {"form": form})
+
+def traitement_enseignant(request):
+    form = EnseignantForm(request.POST)
+    if form.is_valid():
+        Enseignant = form.save()
+        return HttpResponseRedirect("/ecole/liste_enseignants")
+    else:
+        return render(request,"ecole/ajouter_enseignant.html/", {"form":form})
+
+def liste_enseignants(request):
+    liste_enseignants = list(models.Enseignants.objects.all())
+    return render(request, "ecole/liste_enseignants.html", {"liste_en":liste_enseignants})
+
+def afficher_enseignant(request,id):
+    enseignant= models.Enseignants.objects.get(id_ens=id)
+    return render (request, "ecole/afficher_enseignant.html", {"enseignant": enseignant})
+
+def update_enseignant(request,id):
+    enseignant = models.Enseignants.objects.get(id_ens=id)
+    dico= enseignant.dico()
+    form = EnseignantForm(dico)
+    return render(request,"ecole/ajouter_enseignant.html", {"form":form, "id":id})
+
+def traitement_update_enseignant(request, id):
+    enseignant_existant = models.Enseignants.objects.get(id_ens=id)
+    enform = EnseignantForm(request.POST, instance=enseignant_existant)  # Utilisation de instance
+    if enform.is_valid():
+        enseignant = enform.save()
+        return HttpResponseRedirect("/ecole/liste_enseignants")
+    else:
+        return render(request, "ecole/ajouter_enseignant.html", {"enform": enform, "id": id})
+
+def supprimer_enseignant(request,id):
+    enseignant = models.Enseignants.objects.get(id_ens=id)
+    enseignant.delete()
+    return HttpResponseRedirect("/ecole/liste_enseignants")
+
+
+
+
+#Cours
+def ajouter_cours(request):
+    form = CoursForm()
+    return render(request, "ecole/ajouter_cours.html", {"form": form})
+
+def traitement_cours(request):
+    form = CoursForm(request.POST)
+    if form.is_valid():
+        Cours = form.save()
+        return HttpResponseRedirect("/ecole/liste_cours")
+    else:
+        return render(request,"ecole/ajouter_cours.html/", {"form":form})
+
+def liste_cours(request):
+    liste_cours = list(models.Cours.objects.all())
+    return render(request, "ecole/liste_cours.html", {"liste_c":liste_cours})
+
+def afficher_cours(request,id):
+    cours= models.Cours.objects.get(id_cours=id)
+    return render (request, "ecole/afficher_cours.html", {"cours": cours})
+
+def update_cours(request,id):
+    cours = models.Cours.objects.get(id_cours=id)
+    form = CoursForm(instance=cours)
+    return render(request,"ecole/ajouter_cours.html", {"form":form, "id":id})
+
+def traitement_update_cours(request, id):
+    cours_existant = models.Cours.objects.get(id_cours=id)
+    cform = CoursForm(request.POST, instance=cours_existant)  # Utilisation de instance
+    if cform.is_valid():
+        cours = cform.save()
+        return HttpResponseRedirect("/ecole/liste_cours")
+    else:
+        return render(request, "ecole/ajouter_cours.html", {"cform": cform, "id": id})
+
+def supprimer_cours(request,id):
+    cours = models.Cours.objects.get(id_cours=id)
+    cours.delete()
+    return HttpResponseRedirect("/ecole/liste_cours")
+
+
+#Absences
+def ajouter_absence(request):
+    form = AbsenceForm()
+    return render(request, "ecole/ajouter_absence.html", {"form": form})
+
+def traitement_absence(request):
+    form = AbsenceForm(request.POST, request.FILES)
+    if form.is_valid():
+        Absence = form.save()
+        return HttpResponseRedirect("/ecole/liste_absences")
+    else:
+        return render(request,"ecole/ajouter_absence.html/", {"form":form})
+
+def liste_absences(request):
+    liste_absences = list(models.Absences.objects.all())
+    return render(request, "ecole/liste_absences.html", {"liste_ab":liste_absences})
+
+def afficher_absence(request,id):
+    absence= models.Absences.objects.get(id_absence=id)
+    return render (request, "ecole/afficher_absence.html", {"absence": absence})
+
+def update_absence(request,id):
+    absence = models.Absences.objects.get(id_absence=id)
+    form = AbsenceForm(instance=absence)
+    return render(request,"ecole/ajouter_absence.html", {"form":form, "id":id})
+
+def traitement_update_absence(request, id):
+    absence_existant = models.Absences.objects.get(id_absence=id)
+    abform = AbsenceForm(request.POST, request.FILES ,instance=absence_existant)  # Utilisation de instance
+    if abform.is_valid():
+        absence = abform.save()
+        return HttpResponseRedirect("/ecole/liste_absences")
+    else:
+        return render(request, "ecole/ajouter_absence.html", {"abform": abform, "id": id})
+
+def supprimer_absence(request,id):
+    absence = models.Absences.objects.get(id_absence=id)
+    absence.delete()
+    return HttpResponseRedirect("/ecole/liste_absences")
+
+
+def fiche_absence(request, etudiant_id):
+    etudiant = get_object_or_404(Etudiants, pk=etudiant_id)
+    absences = Absences.objects.filter(etudiants=etudiant).select_related('cours')
+
+    return render(request, 'ecole/fiche_absence.html', {
+        'etudiant': etudiant,
+        'absences': absences,
+    })
+
+def liste_absences_cours(request, cours_id):
+    cours = get_object_or_404(Cours, pk=cours_id)
+    absences = Absences.objects.filter(cours=cours).select_related('etudiants')
+
+    return render(request, 'ecole/liste_absences_cours.html', {
+        'cours': cours,
+        'absences': absences,
+    })
